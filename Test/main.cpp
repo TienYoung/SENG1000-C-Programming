6a6fca182d651dbb6f78891d8263d8b2c488c5fd
Tian Yang <tyang2896@conestogac.on.ca>
2024-01-30 12:02:49 -0500

    Test the memory allocation.

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
-	int num = 12;
-	int check = 10;
+	//int num = 12;
+	//int check = 10;
 
-	if ((num && check) == 22)
-	{
-		printf("True\n");
-	}
-	else
-	{
-		printf("false\n");
-	}
-	printf("%d\n", num && check);
+	//if ((num && check) == 22)
+	//{
+	//	printf("True\n");
+	//}
+	//else
+	//{
+	//	printf("false\n");
+	//}
+	//printf("%d\n", num && check);
 
-	char test[] = "Darryl";
-	printf("String is %s\n", test);
-	printf("Array size is %llu\n", sizeof(test));
-	printf("Type(char) size is %lld\n", sizeof(char));
+	//char test[] = "Darryl";
+	//printf("String is %s\n", test);
+	//printf("Array size is %llu\n", sizeof(test));
+	//printf("Type(char) size is %lld\n", sizeof(char));
+
+	char* a = malloc(10);
+
+	for (size_t i = 0; i <10; i++)
+	{
+		a[i] = 'a';
+		printf("%c\n", a[i]);
+	}
+
 
 	return 0;
 }
