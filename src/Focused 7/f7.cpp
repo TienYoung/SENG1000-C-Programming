b620ff287a4310a87a19fd5c7e04b4c8697051d6
Tien Young <tyang2896@conestogac.on.ca>
2024-03-20 16:40:54 -0400

    Add errror chekcing on all file I/O functions.

--- src/Focused 7/f7.cpp
+++ src/Focused 7/f7.cpp
@@ -6,19 +6,57 @@
  * DESCRIPTION :
  * This program practice at creating text and binary files.
  */
-
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	FILE* myTextFile = fopen("myTextFile.txt", "w");
-	fprintf(myTextFile, "This is line 1.\nThis is line 2.\n");
-	fclose(myTextFile);
+	if (myTextFile == NULL)
+	{
+		printf("Can't open or create myTextFile.txt");
+	}
+	else
+	{
+		if (fprintf(myTextFile, "This is line 1.\nThis is line 2.\n") < 0)
+		{
+			printf("Can't write to myTextFile.txt");
+		}
+		else
+		{
+			if (fclose(myTextFile) != 0)
+			{
+				printf("Can't close myTextFile.txt");
+			}
+		}
+	}
 
 	FILE* myEvenDataList = fopen("myEvenDataList.data", "wb");
-	const unsigned short kBinaryData[] = {26946, 24942, 31090, 25632, 29793, 8289, 28518, 8306, 28537, 33141, 39308};
-	fwrite(kBinaryData, sizeof(unsigned short), sizeof(kBinaryData) / sizeof(unsigned short), myEvenDataList);
-	fclose(myEvenDataList);
+	if (myEvenDataList == NULL)
+	{
+		printf("Can't open or create myEvenDataList.data");
+	}
+	else
+	{
+		const unsigned short kBinaryData[] = {
+			26946, 24942, // Bina
+			31090, 25632, // ry d
+			29793, 8289,  // ata
+			28518, 8306,  // for
+			28537, 33141, // you
+			39308};
+		size_t count = sizeof(kBinaryData) / sizeof(unsigned short);
+		if (fwrite(kBinaryData, sizeof(unsigned short), count, myEvenDataList) < count)
+		{
+			printf("Can't write to myEvenDataList.data");
+		}
+		else
+		{
+			if (fclose(myEvenDataList) != 0)
+			{
+				printf("Can't close myEvenDataList.data");
+			}
+		}
+	}
 
 	return 0;
 }
