c51adbf00f79b83f772c9fe02c1e5eb63f2e27c0
Tien Young <tyang2896@conestogac.on.ca>
2024-03-26 12:30:00 -0400

    Indentation: convert tabs to spaces.

--- src/Focused 8/f8.cpp
+++ src/Focused 8/f8.cpp
@@ -13,23 +13,23 @@
 
 typedef struct 
 {
-	int howMany;
-	const char* theText;
-	const char* directoryPath;
-	const char* filename;
+    int howMany;
+    const char* theText;
+    const char* directoryPath;
+    const char* filename;
 } MyDat;
 
 int main(int argc, char* argv[])
 {
-	MyDat myArgs = {strtol(argv[1], NULL, 10), argv[2], argv[3], argv[4]};
-	char filename[40] = "";
-	strcpy(filename, myArgs.directoryPath);
-	FILE* file = fopen(strcat(filename, myArgs.filename), "w");
-	for (int i = 0; i < myArgs.howMany; i++)
-	{
-		fprintf(file, "%s\n", myArgs.theText);
-	}
-	fclose(file);
+    MyDat myArgs = {strtol(argv[1], NULL, 10), argv[2], argv[3], argv[4]};
+    char filename[40] = "";
+    strcpy(filename, myArgs.directoryPath);
+    FILE* file = fopen(strcat(filename, myArgs.filename), "w");
+    for (int i = 0; i < myArgs.howMany; i++)
+    {
+        fprintf(file, "%s\n", myArgs.theText);
+    }
+    fclose(file);
 
-	return 0;
+    return 0;
 }
