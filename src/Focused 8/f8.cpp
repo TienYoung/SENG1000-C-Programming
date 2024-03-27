e97e24d465e335fa0dfa8fc39d72159d1b38fd0e
Tien Young <tyang2896@conestogac.on.ca>
2024-03-26 21:41:39 -0400

    F8: update I/O error checking.
    1. Use defined macro instead of magic number.
    2. Append backslash to directoryPath which suppose not have one.

--- src/Focused 8/f8.cpp
+++ src/Focused 8/f8.cpp
@@ -7,9 +7,13 @@
  * This program practice at working with C-style strings, files, command-line arguments, and structs.
  */
 
+#pragma warning(disable: 4996)
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define MAXLENGTH 41 // 20(directory) + 20(filename) + 1(terminator)
 
 typedef struct 
 {
@@ -21,15 +25,38 @@ typedef struct
 
 int main(int argc, char* argv[])
 {
-    MyDat myArgs = {strtol(argv[1], NULL, 10), argv[2], argv[3], argv[4]};
-    char filename[40] = "";
+    if(argc != 5)
+    {
+        printf("Error: Incorrect number of arguments.\n");
+        return 1;
+    }
+    MyDat myArgs = {
+        .howMany = strtol(argv[1], NULL, 10),
+        .theText = argv[2],
+        .directoryPath = argv[3],
+        .filename = argv[4]};
+        
+    char filename[MAXLENGTH] = "";
     strcpy(filename, myArgs.directoryPath);
+    strcat(filename, "\\");
     FILE* file = fopen(strcat(filename, myArgs.filename), "w");
+    if(file == NULL)
+    {
+        printf("Can't open or create %s\n", filename);
+        return 0;
+    }
     for (int i = 0; i < myArgs.howMany; i++)
     {
-        fprintf(file, "%s\n", myArgs.theText);
+        if(fprintf(file, "%s\n", myArgs.theText) < 0)
+        {
+            printf("Can't write to %s\n", filename);
+            break;
+        }
     }
-    fclose(file);
+    if(fclose(file))
+    {
+        printf("Can't close %s\n", filename);
+    }
 
     return 0;
 }
